d86432d11bf91c2f1c531848f3bf3e4301ecfdf3
Taufik Hidayat <opik.1320@gmail.com>
2024-04-08 22:59:01 +0700

    #1 taufik: fitur - fitur splash screen selesai

---
+++ src/app/splashscreen/splashscreen.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splashscreen',
+  templateUrl: './splashscreen.page.html',
+  styleUrls: ['./splashscreen.page.scss'],
+})
+export class SplashscreenPage implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.router.navigateByUrl('/home');
+    }, 2000);
+  }
+}
